d9f3337af579c8fd8e9afb74a95863f773a400f8
Nikita Zhukov <znp8b00ff@gmail.com>
2025-03-09 03:23:35 +0300

    [SIB-5] add prettier and clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
-    <div className="text-3xl flex justify-center items-center mt-auto">
-            Tailwind TEST
+    <div className='text-3xl flex justify-center items-center mt-auto'>
+      Tailwind TEST
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
