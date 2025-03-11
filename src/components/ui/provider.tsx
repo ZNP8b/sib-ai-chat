b9c17cc58a2162ffb6f71ed14486c5c30d9102e9
Nikita Zhukov <znp8b00ff@gmail.com>
2025-03-12 01:51:38 +0300

    change router to HashRouter

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -2,21 +2,21 @@
 
 import { ChakraProvider, defaultSystem, Theme } from '@chakra-ui/react'
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export function Provider(props: ColorModeProviderProps) {
   const queryClient = new QueryClient()
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider value={defaultSystem}>
-          <Theme colorPalette='purple'>
+          <Theme colorPalette="purple">
             <ColorModeProvider {...props} />
           </Theme>
         </ChakraProvider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
