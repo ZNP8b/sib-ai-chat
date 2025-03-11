e2a9571491e57fd76f9e26a8a8a1616d00f62cae
Nikita Zhukov <nikita@evrone.team>
2025-03-11 11:17:52 +0300

    test custom api url (ngrok)

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,59 +1,71 @@
-import { Redirect } from '@/components/Redirect/redirect.tsx'
-import { StarterPasswordForm } from '@/components/starter-password-form/starter-password-form.tsx'
-import { Button, CloseButton, Drawer, Portal } from '@chakra-ui/react'
-import { useState } from 'react'
+import {Redirect} from '@/components/redirect/redirect.tsx'
+import {StarterPasswordForm} from '@/components/starter-password-form/starter-password-form.tsx'
+import {Button, CloseButton, Drawer, Portal} from '@chakra-ui/react'
+import {useEffect, useState} from 'react'
 
 export function Home() {
-  const isAuth = localStorage.getItem('isAuth')
-  const apiUrl = localStorage.getItem('apiUrl')
+    const [isAuth] = useState(() => localStorage.getItem('isAuth'))
+    const [apiUrl] = useState(() => localStorage.getItem('apiUrl'))
 
-  const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
 
-  return (
-    <>
-      {isAuth ? (
-        <Redirect url={apiUrl ? '/' : '/settings'} />
-      ) : (
-        <StarterPasswordForm />
-      )}
+    useEffect(() => {
+        getPing().then(json => json)
 
-      <Drawer.Root
-        open={open}
-        onOpenChange={e => setOpen(e.open)}
-        placement='start'
-      >
-        <div>
-          <Drawer.Trigger asChild>
-            <Button variant='outline' size='lg'>
-              Menu
-            </Button>
-          </Drawer.Trigger>
-        </div>
+    }, []);
 
-        <Portal>
-          <Drawer.Backdrop />
-          <Drawer.Positioner>
-            <Drawer.Content>
-              <Drawer.Header>
-                <Drawer.Title>Drawer Title</Drawer.Title>
-              </Drawer.Header>
-              <Drawer.Body>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </Drawer.Body>
-              <Drawer.Footer>
-                <Button variant='outline'>Cancel</Button>
-                <Button>Save</Button>
-              </Drawer.Footer>
-              <Drawer.CloseTrigger asChild>
-                <CloseButton size='sm' />
-              </Drawer.CloseTrigger>
-            </Drawer.Content>
-          </Drawer.Positioner>
-        </Portal>
-      </Drawer.Root>
-    </>
-  )
+    const getPing = async () => {
+        const response = await fetch(apiUrl + '/api/ping', {
+            method: "GET",
+            headers: {
+                "ngrok-skip-browser-warning": "69420",
+            },
+        });
+        return await response.json()
+    }
+
+    return (
+        <>
+            {isAuth ? (
+                <Redirect url={apiUrl ? '/' : '/settings'}/>
+            ) : (
+                <StarterPasswordForm/>
+            )}
+
+            <Drawer.Root
+                open={open}
+                onOpenChange={e => setOpen(e.open)}
+                placement='start'
+            >
+                <div>
+                    <Drawer.Trigger asChild>
+                        <Button variant='outline' size='lg'>
+                            Menu
+                        </Button>
+                    </Drawer.Trigger>
+                </div>
+
+                <Portal>
+                    <Drawer.Backdrop/>
+                    <Drawer.Positioner>
+                        <Drawer.Content>
+                            <Drawer.Header>
+                                <Drawer.Title>Drawer Title</Drawer.Title>
+                            </Drawer.Header>
+                            <Drawer.Body>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </Drawer.Body>
+                            <Drawer.Footer></Drawer.Footer>
+                            <Drawer.CloseTrigger asChild>
+                                <CloseButton size='sm'/>
+                            </Drawer.CloseTrigger>
+                        </Drawer.Content>
+                    </Drawer.Positioner>
+                </Portal>
+            </Drawer.Root>
+        </>
+    )
 }
