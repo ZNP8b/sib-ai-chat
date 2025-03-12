950a1d5f0ac03f540f9258bd0a71bd8bd8336a59
Nikita Zhukov <znp8b00ff@gmail.com>
2025-03-13 01:16:08 +0300

    [SIB-19] create capec 2 cve page

--- src/components/layout/route-list.tsx
+++ src/components/layout/route-list.tsx
@@ -2,13 +2,8 @@ import { Route, Routes } from 'react-router'
 import App from '@/components/app/App.tsx'
 import { Settings } from '@/components/settings/settings.tsx'
 import { Home } from '@/components/home/home.tsx'
-import DashboardPage from '@/pages/DashboardPage.tsx'
-import DatasetsPage from '@/pages/DatasetsPage.tsx'
-import CheckpointsPage from '@/pages/CheckpointsPage.tsx'
-import TrainingPage from '@/pages/TrainingPage.tsx'
-import LoadModelPage from '@/pages/LoadModelPage.tsx'
-import InferencePage from '@/pages/InferencePage.tsx'
 import { StartingPage } from '@/pages/StartingPage.tsx'
+import { Capec2CVEPage } from '@/pages/Capec2CVEPage.tsx'
 
 export function RouteList() {
   return (
@@ -17,12 +12,13 @@ export function RouteList() {
         <Route index element={<Home />} />
         <Route path='/start' element={<StartingPage />} />
         <Route path='/settings' element={<Settings />} />
-        <Route path='/dashboard' element={<DashboardPage />} />
-        <Route path='/datasets' element={<DatasetsPage />} />
-        <Route path='/checkpoints' element={<CheckpointsPage />} />
-        <Route path='/training' element={<TrainingPage />} />
-        <Route path='/load-model' element={<LoadModelPage />} />
-        <Route path='/inference' element={<InferencePage />} />
+        <Route path='/capec2cve' element={<Capec2CVEPage />} />
+        {/*<Route path='/dashboard' element={<DashboardPage />} />*/}
+        {/*<Route path='/datasets' element={<DatasetsPage />} />*/}
+        {/*<Route path='/checkpoints' element={<CheckpointsPage />} />*/}
+        {/*<Route path='/training' element={<TrainingPage />} />*/}
+        {/*<Route path='/load-model' element={<LoadModelPage />} />*/}
+        {/*<Route path='/inference' element={<InferencePage />} />*/}
       </Route>
     </Routes>
   )
