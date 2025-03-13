4a64196d31391dd7d9c5fb63739cea40602d9c81
Nikita Zhukov <znp8b00ff@gmail.com>
2025-03-14 01:49:22 +0300

    [SIB-22] add pagination

--- src/pages/Capec2CVEPage.tsx
+++ src/pages/Capec2CVEPage.tsx
@@ -4,19 +4,22 @@ import { NvdCveResponse } from '@/components/capec-to-cve-components/types.tsx'
 import { Capec2CWEForm } from '@/components/capec-to-cve-components/Capec2CWEForm.tsx'
 import { CWE2CVEForm } from '@/components/capec-to-cve-components/CWE2CVEForm.tsx'
 import { CveCardList } from '@/components/capec-to-cve-components/cve-card-list.tsx'
-import { useCVEMutation } from '@/components/capec-to-cve-components/data.ts'
+import { useCVEForm, useCVEMutation } from '@/components/capec-to-cve-components/data.ts'
 
 export function Capec2CVEPage() {
   const [cweIds, setCWEIds] = useState<string[]>([])
   const [CVEs, setCVEs] = useState<NvdCveResponse>()
+  const [page, setPage] = useState(1)
 
-  const cveMutation = useCVEMutation(setCVEs)
+  const { register, handleSubmit, errors, setError, getValues } = useCVEForm()
+
+  const cveMutation = useCVEMutation(setCVEs, setError)
 
   return (
     <Container>
       <Capec2CWEForm setCWEIds={setCWEIds} />
 
-      <Box marginTop='12px'>
+      <Box marginTop="12px">
         {cweIds.map((cweId, index) => (
           <span key={cweId}>
             {cweId}
@@ -25,8 +28,9 @@ export function Capec2CVEPage() {
         ))}
       </Box>
 
-      <CWE2CVEForm setCVEs={setCVEs} />
-      <CveCardList CVEs={CVEs} mutation={cveMutation} />
+      <CWE2CVEForm mutation={cveMutation} register={register} handleSubmit={handleSubmit} errors={errors}
+                   setCVEs={setCVEs} />
+      <CveCardList page={page} setPage={setPage} getValues={getValues} CVEs={CVEs} mutation={cveMutation} />
     </Container>
   )
 }
